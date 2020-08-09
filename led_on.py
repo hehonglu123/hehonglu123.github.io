bc8cf9ae21083c0cb8bf460e66fdd996a694f70a
hehonglu123 <hehonglu123@126.com>
2020-08-09 15:02:39 -0400

    update ip

--- led_on.py
+++ led_on.py
@@ -3,9 +3,10 @@ from RobotRaconteur.Client import *
 import traceback
 from js import print_div
 ####################Start Service and robot setup
+ip='128.213.64.202'
 async def led():
 	try:
-		m1k_obj=await RRN.AsyncConnectService('rr+ws://localhost:11111/?service=m1k',None,None,None,None)
+		m1k_obj=await RRN.AsyncConnectService('rr+ws://'+ip+'/?service=m1k',None,None,None,None)
 
 		m1k_obj.async_setled(7,None)
 	except:
